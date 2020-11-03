f9b25bae332cc11cb34919efc8f92786235938a7
Andrea Borruso <aborruso@gmail.com>
2020-11-03 21:54:11 +0100

    script elaborazione dati 20201102

---
+++ 082053/script/20201102.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# rimuovi da output CSV di tabula
+mlr --csv -N cat \
+  then remove-empty-columns \
+  then cut -x -r -f "[0-9]{2}" \
+  then cut -x -f 8 "$folder"/../rawdata/tabula-20201102-Covid_PA_201102.csv >"$folder"/../output/20201102-Covid_PA.csv
+
+# ristruttira CSV da wide a long
+mlr -I --csv reshape -r "/" -o item,value \
+  then rename item,data,value,positivi \
+  then put -S '$data=sub($data,"([^/]+)/([^/]+)","2020-\2-\1");$data = strftime(strptime($data, "%Y-%m-%d"),"%Y-%m-%d")' \
+  then sort -f data,COMUNE "$folder"/../output/20201102-Covid_PA.csv
