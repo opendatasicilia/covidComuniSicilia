c50c563e21fc582c359b9214adeee11e8ab48cfc
Andrea Borruso <aborruso@gmail.com>
2020-11-20 16:52:10 +0100

    update

--- script/positiviProvinciaPalermo/positiviProvinciaPalermo.sh
+++ script/positiviProvinciaPalermo/positiviProvinciaPalermo.sh
@@ -21,7 +21,7 @@ code=$(curl -s -L -o /dev/null -w "%{http_code}" ''"$URL"'')
 if [ $code -eq 200 ]; then
   curl -kL "$URL" >"$folder"/rawdata/positiviProvinciaPalermo.xlsx
   in2csv -I --sheet tavola_pop_res01 "$folder"/rawdata/positiviProvinciaPalermo.xlsx >"$folder"/rawdata/positiviProvinciaPalermo.csv
-  mlr --csv -N filter -S '$1=~"^(8|Pr)"' rawdata/positiviProvinciaPalermo.csv >"$folder"/processing/positiviProvinciaPalermo.csv
+  mlr --csv -N filter -S '$1=~"^(8|Pr)"' "$folder"/rawdata/positiviProvinciaPalermo.csv >"$folder"/processing/positiviProvinciaPalermo.csv
   sed -i -r 's/(\.|…)+//g' "$folder"/processing/positiviProvinciaPalermo.csv
   mv "$folder"/processing/positiviProvinciaPalermo.csv "$folder"/../../082053/output/positiviProvinciaPalermo.csv
   mlr --csv remove-empty-columns then reshape -r "-" -o item,value then sort -f "Pro Com",item then rename item,data,value,positivi "$folder"/../../082053/output/positiviProvinciaPalermo.csv >"$folder"/../../082053/output/positiviProvinciaPalermoLong.csv
